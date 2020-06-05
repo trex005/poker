d13eed2eb23ef419abc4963fbc0a1a12002949a7
Travis Harris <travis@tapy.com>
2020-06-05 12:57:03 -0400

    remove debug

--- poker_modules/player.js
+++ poker_modules/player.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcrypt');
 const squel = require("squel");
 const spg = squel.useFlavour('postgres');
 const { Pool } = require('pg');
-console.log(process.env.DATABASE_URL);
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.DATABASE_URL.indexOf('compute-1.amazonaws.com') === -1 ? false : true
